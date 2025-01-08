99f474e2b51c1bf5e8dd1a25295dfe925eb1ca59
Sathish-Kumar-E <sathishkumar13121998@gmail.com>
2025-01-08 15:54:29 -0500

    107 Binary Tree Level Order Traversal II , Solution 2

--- Java/107_Binary_Tree_Level_Order_Traversal_II.java
+++ Java/107_Binary_Tree_Level_Order_Traversal_II.java
@@ -18,3 +18,29 @@ class Solution {
         return result;
     }
 }
+
+
+class Solution {
+    private List<List<Integer>> result = new LinkedList<>();
+    private Queue<TreeNode> queue = new LinkedList<>();
+    private Stack<List<Integer>> stack = new Stack<List<Integer>>();
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        if(root == null) return result;
+        queue.add(root);
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> list = new LinkedList<>();
+            for(int i = 0 ; i < size ; i++) {
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if(node.left != null) queue.add(node.left);
+                if(node.right != null) queue.add(node.right);
+            }
+            stack.push(list);
+        }
+        while(!stack.isEmpty()) {
+            result.add(stack.pop());
+        }
+        return result;
+    }
+}
